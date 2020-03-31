17eae29e187547c5c32ddd96dd38ba4d58bbbb7f
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-04-01 00:07:20 +0200

    Pegelverwaltung mit HashMap aber noch nicht ganz fertig.

--- src/Pegelverwaltung/WasserstandApp.java
+++ src/Pegelverwaltung/WasserstandApp.java
@@ -4,10 +4,15 @@ public class WasserstandApp {
     public static void main(String[] args) {
         Wasserstandmanager ws = new Wasserstandmanager();
 
-        ws.addWasserstand(new Wasserstand(3145,"Lahne", 2.3, 3.1, 34567890));
+        ws.addWasserstand(new Wasserstand(3145,"Lahne", 2.3, 3.1, 3456));
+        ws.addWasserstand(new Wasserstand(3146,"Lahne", 3.3, 3.1, 3460));
+        ws.addWasserstand(new Wasserstand(3147,"Donau", 4.3, 5.1, 3480));
         System.out.println(ws.toString());
         System.out.println(ws.finById(3145));
-        System.out.println(ws.findByGewaesser("Lahne"));
+        System.out.println("Gewaesser nach Name: " + ws.findByGewaesser("Lahne"));
+        System.out.println("Höchstwasser: " + ws.findHoechstWasserstand("Lahne"));
+        System.out.println("Wasserstandssummen: " + ws.getSummeWasserAlleGewaesser());
+
 
 
     }
--- src/Pegelverwaltung/Wasserstandmanager.java
+++ src/Pegelverwaltung/Wasserstandmanager.java
@@ -1,6 +1,7 @@
 package Pegelverwaltung;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Wasserstandmanager {
     private ArrayList<Wasserstand> wasserstandListe = new ArrayList<>();
@@ -21,10 +22,38 @@ public class Wasserstandmanager {
         ArrayList<Wasserstand> gewaesserListe = new ArrayList<>();
         for (Wasserstand w:wasserstandListe){
             String gewaesser = w.getGewaesserName();
-            if(gewaesser.equals(w.getGewaesserName())){
+            if(gewaesser.equals(gewaesserName)){
                 gewaesserListe.add(w);
             }
         }return gewaesserListe;
+    }
+
+    public Wasserstand findHoechstWasserstand (String gewaesserName){
+        ArrayList<Wasserstand> gewaesserListe = findByGewaesser(gewaesserName);
+        double wasserst = 0;
+        for(Wasserstand w:gewaesserListe){
+            if(wasserst<w.getMesswert()){
+                wasserst = w.getMesswert();
+            }
+        }
+        for(Wasserstand w:gewaesserListe){
+            if(wasserst == w.getMesswert()){
+                return w;
+            }
+        } return null;
+    }
+
+    public HashMap<String,Double> getSummeWasserAlleGewaesser(){
+        HashMap<String,Double> summeWasserstaende = new HashMap<>();
+        for(Wasserstand w:wasserstandListe){
+            double summe;
+            if(summeWasserstaende.containsKey(w.getGewaesserName())){
+                 summe = summeWasserstaende.get(w.getGewaesserName());
+                 summe = summe + w.getMesswert();
+                 summeWasserstaende.put(w.getGewaesserName(),summe);
+            }else{summeWasserstaende.put(w.getGewaesserName(),w.getMesswert());}
+        }
+        return summeWasserstaende;
     }
 
     @Override
